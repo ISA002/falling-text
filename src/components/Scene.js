5610df113436e0771299ef958e2cf9877a5cf4c5
Ivan Solovyov <iasolovyovwork@gmail.com>
2021-04-22 18:18:28 +0300

    Add drag controls

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 // import gsap from 'gsap';
 import fontSculpt from "../assets/fonts/sculpture.typeface.json";
+import { DragControls } from 'three/examples/jsm/controls/DragControls';
 import C from "cannon";
 
 const distance = 15;
@@ -19,7 +20,12 @@ export default class Renderer3D {
     this.renderer.setClearColor(0x111111, 1);
     dom.appendChild(this.renderer.domElement);
 
-    this.textList = ["MESSAGE", "SIMPLE", "THREE", "REACT", "CANNON", "MESSAGE", "SIMPLE", "THREE", "REACT", "CANNON", "MESSAGE", "SIMPLE", "THREE", "REACT", "CANNON", "MESSAGE", "SIMPLE", "MESSAGE", "SIMPLE", "THREE", "REACT", "CANNON", "MESSAGE", "SIMPLE", "THREE", "REACT", "CANNON", "MESSAGE", "SIMPLE", "THREE", "REACT", "CANNON", "MESSAGE", "SIMPLE"];
+    this.textList = ["VISUAL", "JAVASCRIPT",
+    //  "THREE", "REACT", "CANNON", "VISUAL", "JAVASCRIPT",
+    //   "THREE", "REACT", "CANNON", "VISUAL", "JAVASCRIPT", "THREE", "REACT", "CANNON", "VISUAL",
+    //   "JAVASCRIPT", "VISUAL", "JAVASCRIPT", "THREE", "REACT", "CANNON", "VISUAL", "JAVASCRIPT", "THREE",
+    //   "REACT", "CANNON", "VISUAL", "JAVASCRIPT", "THREE", "REACT", "CANNON", "VISUAL", "JAVASCRIPT",
+    ];
 
     const aspect = this.width / this.height;
 
@@ -32,19 +38,19 @@ export default class Renderer3D {
       100
     );
 
-    this.camera.position.set(0, 0, 0)
-    this.camera.lookAt(new THREE.Vector3())
+    this.camera.position.set(0, 0, 0);
+    this.camera.lookAt(new THREE.Vector3());
     // this.time = 0;
 
-    this.isPlaying = 0;
     this.mouse = { x: 0, y: 0 };
 
     this.words = [];
+    this.dragging = false;
+    this.draggingId = null;
 
     this.world = new C.World();
-    this.world.gravity.set(0, -15, 0);
+    this.world.gravity.set(0, -20, 0);
 
-    this.addListeners();
     this.addObjects();
     this.render();
   }
@@ -70,7 +76,6 @@ export default class Renderer3D {
     // const loader = new THREE.FontLoader();
     const fontsss = new THREE.Font(fontSculpt);
     // loader.load(JSON.stringify(fontsss), (font) => {
-    //   console.log("log", font);
 
     const groundMat = new C.Material();
     const letterMat = new C.Material();
@@ -81,7 +86,7 @@ export default class Renderer3D {
 
     this.world.addContactMaterial(contactMaterial);
 
-    const color = 0xFFFFFF;
+    const color = 0xffffff;
     const matLite = new THREE.MeshBasicMaterial({
       color: color,
       transparent: true,
@@ -89,39 +94,39 @@ export default class Renderer3D {
       side: THREE.DoubleSide,
     });
 
-    const words = new THREE.Group();
-    words.ground = new C.Body({
+    this.wordsGroup = new THREE.Group();
+    this.wordsGroup.ground = new C.Body({
       mass: 0,
       shape: new C.Box(new C.Vec3(100, 50, 20)),
       position: new C.Vec3(10, -65, -10),
       material: groundMat,
     });
 
-    words.wallLeft = new C.Body({
+    this.wordsGroup.wallLeft = new C.Body({
       mass: 0,
       shape: new C.Box(new C.Vec3(10, 100, 20)),
       position: new C.Vec3(-35, -59, -10),
       material: groundMat,
     });
 
-    words.wallRight = new C.Body({
+    this.wordsGroup.wallRight = new C.Body({
       mass: 0,
       shape: new C.Box(new C.Vec3(10, 100, 20)),
       position: new C.Vec3(35, -59, -10),
       material: groundMat,
     });
 
-    this.world.addBody(words.ground);
-    this.world.addBody(words.wallLeft);
-    this.world.addBody(words.wallRight);
+    this.world.addBody(this.wordsGroup.ground);
+    this.world.addBody(this.wordsGroup.wallLeft);
+    this.world.addBody(this.wordsGroup.wallRight);
 
     for (let i = 0; i < this.textList.length; i++) {
-      const message = this.textList[i];
+      const VISUAL = this.textList[i];
 
-      const geometry = new THREE.TextGeometry(message, {
+      const geometry = new THREE.TextGeometry(VISUAL, {
         font: fontsss,
         size: 2,
-        height: 50000,
+        height: 100000,
         curveSegments: 24,
         bevelEnabled: false,
         bevelThickness: 0.9,
@@ -130,13 +135,8 @@ export default class Renderer3D {
         bevelSegments: 10,
       });
 
-      // const shapes = fontsss.generateShapes(message, 2);
-      // const geometry = new THREE.ShapeGeometry(shapes);
       geometry.computeBoundingBox();
       geometry.computeBoundingSphere();
-      // const xMid =
-      //   -0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
-      // geometry.translate(-15 + i * 7, i * 2, 0);
 
       const text = new THREE.Mesh(geometry, matLite);
 
@@ -147,39 +147,26 @@ export default class Renderer3D {
 
       text.body = new C.Body({
         mass: 100,
-        position: new C.Vec3(-4 + (-1)**i * 5, 6 * i, 0),
+        position: new C.Vec3(-4 + (-1) ** i * 5, 6 * i, 0),
         material: letterMat,
       });
 
       const { center } = text.geometry.boundingSphere;
       text.body.addShape(box, new C.Vec3(center.x, center.y, center.z));
 
-      words.add(text);
+      this.wordsGroup.add(text);
       this.world.addBody(text.body);
     }
 
-    // const axesHelper = new THREE.AxesHelper( 10 );
-    // this.scene.add( axesHelper );
-
-    this.words.push(words);
-    this.scene.add(words);
+    this.words.push(this.wordsGroup);
+    this.controls = new DragControls(this.words, this.camera, this.renderer.domElement);
+    this.scene.add(this.wordsGroup);
+    this.scene.add(this.controls);
+    this.addListeners();
     // });
   };
 
-  stop = () => {
-    this.isPlaying = false;
-  };
-
-  play = () => {
-    if (!this.isPlaying) {
-      this.render();
-      this.isPlaying = true;
-    }
-  };
-
   render = () => {
-    if (this.isPlaying) return;
-
     this.updatePhisics();
     this.world.step(1 / 60);
 
@@ -191,16 +178,65 @@ export default class Renderer3D {
     if (!this.words) return;
 
     this.words[0].children.forEach((word) => {
+      if (word.id === this.draggingId) return;
       word.position.copy(word.body.position);
       word.quaternion.copy(word.body.quaternion);
-      // word.quaternion._z = 0;
-      // word.quaternion._w = 1; 
       word.position.z = 0;
       word.rotation._z = 0;
       word.rotation._y = 0;
       word.rotation._x = 0;
     });
   };
+
+  addNewWord = () => {
+    const fontsss = new THREE.Font(fontSculpt);
+    const letterMat = new C.Material();
+
+    const color = 0x9910ff;
+    const matLite = new THREE.MeshBasicMaterial({
+      color: color,
+      transparent: true,
+      opacity: 1,
+      side: THREE.DoubleSide,
+    });
+
+    const VISUAL = this.textList[0];
+
+    const geometry = new THREE.TextGeometry(VISUAL, {
+      font: fontsss,
+      size: 2,
+      height: 50000,
+      curveSegments: 24,
+      bevelEnabled: false,
+      bevelThickness: 0.9,
+      bevelSize: 0.3,
+      bevelOffset: 0,
+      bevelSegments: 10,
+    });
+
+    geometry.computeBoundingBox();
+    geometry.computeBoundingSphere();
+
+    const text = new THREE.Mesh(geometry, matLite);
+
+    text.position.set(this.mouse.x * 30, this.mouse.y * 15, 0);
+    text.size = text.geometry.boundingBox.getSize(new THREE.Vector3());
+
+    const box = new C.Box(new C.Vec3().copy(text.size).scale(0.6));
+
+    text.body = new C.Body({
+      mass: 100,
+      position: new C.Vec3(this.mouse.x * 30, this.mouse.y * 15, 0),
+      material: letterMat,
+    });
+
+    const { center } = text.geometry.boundingSphere;
+    text.body.addShape(box, new C.Vec3(center.x, center.y, center.z));
+    text.addEventListener('drag', this.handleDrag)
+
+    this.wordsGroup.add(text);
+    this.world.addBody(text.body);
+  }
 
   onResize = () => {
     this.width = window.innerWidth;
@@ -217,9 +253,12 @@ export default class Renderer3D {
   };
 
   handleClick = () => {
-    const value = this.textList.pop();
-    this.textList.unshift(value);
-    this.words[0].children.pop();
-    this.world.bodies.pop()
+    if (!this.dragging) {
+      const value = this.textList.pop();
+      this.textList.unshift(value);
+      this.words[0].children.pop();
+      this.world.bodies.pop();
+      this.addNewWord();
+    }
   };
 }
